ecd40bfb676f67bac8125bdfe6863e49540b41ca
Eng-NUREDDIN <nour.hammoda@gmail.com>
2023-03-19 00:53:12 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Code your solutions in this file
+//let names = ['name1','name2','name3'];
+function writeCards(names){
+    let thanksArray=[];
+  for(let i=0;i<names.length;i++){      
+      thanksArray.push('Thank you, '+names[i]+', for the wonderful surprise gift!');      
+  }
+    return thanksArray;
+  }
+function countDown(number){
+    for(let num=number;num>=0;num--){
+        console.log(num);
+    }
+}
